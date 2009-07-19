1ed9b8dcb375cb6ad235e7c7f25d206fc1195b7c
Jeff Coleman <progressions@gmail.com>
2009-07-19 10:25:11 -0500

    Support multiple sites

--- monitor.rb
+++ monitor.rb
@@ -5,16 +5,28 @@ require 'json'
 require 'httparty'
 
 @file = "monitor.js"
-@url = 'http://www.weracketeer.com'
-   
+ 
 while true do
-  File.open(@file, "w") do |f|
-    response = HTTParty.get(@url)
-    f.write JSON.generate({
-      :url => @url,
-      :status => response.code
-    })
-    puts "writing"
+  @sites = YAML.load_file("monitor.yml")["sites"]
+
+  @sites.each do |site|
+    puts site[1].inspect
+    
+    @url = site[1]["url"]
+    
+    if @url !~ /^http/
+      @url = "http://#{@url}"
+    end
+    
+    File.open(@file, "w") do |f|
+      response = HTTParty.get(@url)
+      @result = JSON.generate({
+        :url => @url,
+        :status => response.code
+      })
+      f.write(@result)
+      puts "pinging #{@url} with result: #{@result}"
+    end
   end
   sleep(5)
 end
